ab3b33b9777cb5832c367225781f00a77f1c86b9
Atomsk <AtomskJD@gmail.com>
2012-08-28 16:31:45 +0600

    old update

---
+++ TEST/dieTest.php
@@ -0,0 +1,8 @@
+<?php
+	$var = false;
+    $far = false;
+    echo $far and 3 or die('hehe');
+//    isset($var) or die('COOL its dead');
+    $var or die('COOL its dead');
+    echo 'NOT COOL';
+?>
